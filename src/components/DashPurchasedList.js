76299757d2120db208138f53314906b6f220c263
pipeline <pipeline@nuance.com>
2023-02-07 10:35:06 +0530

    Added files for client side

---
+++ src/components/DashPurchasedList.js
@@ -0,0 +1,204 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+// MUI
+import {
+  Box,
+  ButtonGroup,
+  Table,
+  TableRow,
+  TableCell,
+  TableHead,
+  TableBody,
+  Button,
+} from '@mui/material';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+// Styling
+import {
+  paginationStyle,
+  purchasedListContainerStyle,
+  purchasedListTableStyle,
+} from './css/dashStyle';
+// Api Call
+import { addReview } from '../actions/ad';
+
+const DashPurchasedList = (props) => {
+  const { ads } = props;
+  const navigate = useNavigate();
+  const [pageNumber, setPageNumber] = useState(1);
+  const [adPerPage] = useState(5);
+  const [open, setOpen] = useState(false);
+  const [adId, setAdId] = useState();
+  const [ownerId, setOwnerId] = useState();
+  const [comment, setComment] = useState("");
+  const [disableBtn, setDisableBtn] = useState(false);
+
+  const getGMTTime = (time) => {
+    const dateTime = new Date(time);
+    return dateTime.toUTCString();
+  };
+
+  const handlePurchasedDetails = (adId) => {
+    navigate('/ads/' + adId);
+  };
+
+  // Modal for Review
+  const handleReviewOpen = (adId, ownerId) => {
+    setOpen(true);
+    setAdId(adId);
+    setOwnerId(ownerId);
+  };
+  const handleReviewClose = () => {
+    setOpen(false);
+  };
+  const handleReviewSubmit = () => {
+    props.addReview(adId, comment, ownerId);
+    setDisableBtn(true);
+    setOpen(false);
+  };
+
+  // Pagination
+  let lastAdIndex = pageNumber * adPerPage;
+  let firstAdIndex = lastAdIndex - adPerPage;
+  let pageNumbers = [];
+  const num = Math.ceil(ads.length / adPerPage);
+  for (let i = 1; i <= num; i++) {
+    pageNumbers.push(i);
+  }
+  const clickPageNumberButton = (num) => {
+    setPageNumber(num);
+  };
+
+  return (
+    <>
+      <Dialog open={open} onClose={handleReviewClose}>
+        <DialogTitle>Add Review</DialogTitle>
+        <DialogContent>
+
+          <DialogContentText>
+            Add the experience for product you purchased.
+          </DialogContentText>
+
+          <TextField
+            autoFocus
+            margin="dense"
+            id="product_review"
+            label="Review"
+            type="text"
+            fullWidth
+            variant="standard"
+            onChange={(newValue) => { setComment(newValue.target.value) }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleReviewClose}>Cancel</Button>
+          <Button onClick={handleReviewSubmit}>Submit</Button>
+        </DialogActions>
+      </Dialog>
+      {ads.length > 0 ? <Box sx={purchasedListContainerStyle}>
+        <Box sx={purchasedListTableStyle}>
+          <Table sx={{ width: '70%', minWidth: 650 }} aria-label='simple table'>
+            <TableHead>
+              <TableRow>
+                <TableCell>Product name</TableCell>
+                <TableCell align='right'>Price</TableCell>
+                <TableCell align='right'>Date</TableCell>
+                <TableCell align='right'>Details</TableCell>
+                <TableCell align='right'>Feedback</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {ads.slice(firstAdIndex, lastAdIndex).map((ad) => (
+                <TableRow key={ad._id}>
+                  <TableCell>{ad.productName}</TableCell>
+                  <TableCell align='right'>${ad.currentPrice.$numberDecimal}</TableCell>
+                  <TableCell align='right'>{getGMTTime(ad.updatedAt)}</TableCell>
+                  <TableCell align='right'>
+                    <Button
+                      size='small'
+                      variant='outlined'
+                      onClick={(e) => {
+                        handlePurchasedDetails(ad._id);
+                      }}
+                    >
+                      Details
+                    </Button>
+                  </TableCell>
+                  <TableCell align='right'>
+                    {ad.review.length > 0 || disableBtn ? <Button
+                      size='small'
+                      variant='outlined'
+                      onClick={(e) => {
+                        handleReviewOpen(ad._id, ad.owner);
+                      }}
+                      disabled
+                    >
+                      Review
+                    </Button>
+                      :
+                      <Button
+                        size='small'
+                        variant='outlined'
+                        onClick={(e) => {
+                          handleReviewOpen(ad._id, ad.owner);
+                        }}
+                      >
+                        Review
+                      </Button>
+                    }
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Box>
+        {ads.length !== 0 && (
+          <Box sx={paginationStyle}>
+            <ButtonGroup variant='outlined' size='small'>
+              <Button
+                disabled={pageNumber === 1}
+                onClick={(e) => clickPageNumberButton(pageNumber - 1)}
+              >
+                Prev
+              </Button>
+              {pageNumbers.map((num) => {
+                return (
+                  <Button
+                    key={num}
+                    disabled={pageNumber === num}
+                    onClick={(e) => clickPageNumberButton(num)}
+                  >
+                    {num}
+                  </Button>
+                );
+              })}
+              <Button
+                disabled={pageNumber === pageNumbers[pageNumbers.length - 1]}
+                onClick={(e) => clickPageNumberButton(pageNumber + 1)}
+              >
+                Next
+              </Button>
+            </ButtonGroup>
+          </Box>
+        )}
+      </Box> : <Box sx={purchasedListTableStyle}>No Purchased Product Found</Box>}
+
+    </>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  loading: state.auth.loading,
+  isAuth: state.auth.isAuthenticated,
+  user: state.auth.user,
+  purchased: state.ad.purchased,
+  purchasedLoading: state.ad.purchasedLoading,
+});
+
+export default connect(mapStateToProps, { addReview })(DashPurchasedList);
---
+++ src/components/DashboardReviewList.js
@@ -0,0 +1,120 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import axios from 'axios';
+// Components
+import LoadingDisplay from './LoadingDisplay';
+import Card from './Card';
+import { Button, Box, ButtonGroup } from '@mui/material';
+// Styling
+import { paginationStyle, purchasedListContainerStyle, purchasedListTableStyle } from './css/dashStyle';
+
+// MUI
+import {
+    Table,
+    TableRow,
+    TableCell,
+    TableHead,
+    TableBody,
+} from '@mui/material';
+
+const DashboardReviewList = () => {
+    const [ads, setAds] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [pageNumber, setPageNumber] = useState(1);
+    const [adPerPage] = useState(4);
+
+    useEffect(() => {
+        setLoading(true);
+        let soldWithReviewList = [];
+
+        const fetchData = async () => {
+            const res = await axios.get(
+                `${process.env.REACT_APP_API_BASE_URL}/user/products/posted`
+            );
+            for (let i = 0; i < res.data.length; i++) {
+                if (res.data[i].sold && res.data[i].review != "") {
+                    soldWithReviewList.push(res.data[i]);
+                }
+            }
+            setAds(soldWithReviewList);
+            setLoading(false);
+        };
+        fetchData();
+    }, []);
+
+    // Pagination
+    let lastAdIndex = pageNumber * adPerPage;
+    let firstAdIndex = lastAdIndex - adPerPage;
+    let pageNumbers = [];
+    const num = Math.ceil(ads.length / adPerPage);
+    for (let i = 1; i <= num; i++) {
+        pageNumbers.push(i);
+    }
+    const clickPageNumberButton = (num) => {
+        setPageNumber(num);
+    };
+
+    return loading ? (
+        <LoadingDisplay />
+    ) : (
+        <Fragment>
+            {ads.length > 0 ?
+                <Box sx={purchasedListContainerStyle}>
+                    <Box sx={purchasedListTableStyle}>
+                        <Table sx={{ width: '70%', minWidth: 650 }} aria-label='simple table'>
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell>Product name</TableCell>
+                                    <TableCell align='right'>Review</TableCell>
+                                    <TableCell align='right'>Rating</TableCell>
+                                </TableRow>
+                            </TableHead>
+                            <TableBody>
+                                {ads.slice(firstAdIndex, lastAdIndex).map((ad) => (
+                                    <TableRow key={ad._id}>
+                                        <TableCell>{ad.productName}</TableCell>
+                                        <TableCell align='right'>{ad.review}</TableCell>
+                                        {ad.rating ? <TableCell align='right'>{ad.rating}</TableCell> : <TableCell align='right'>{0}</TableCell>}
+                                        {/* <TableCell align='right'>{ad.rating}</TableCell> */}
+                                    </TableRow>
+                                ))}
+                            </TableBody>
+                        </Table>
+                    </Box>
+                    {ads.length !== 0 && (
+                        <Box sx={paginationStyle}>
+                            <ButtonGroup variant='outlined' size='small'>
+                                <Button
+                                    disabled={pageNumber === 1}
+                                    onClick={(e) => clickPageNumberButton(pageNumber - 1)}
+                                >
+                                    Prev
+                                </Button>
+                                {pageNumbers.map((num) => {
+                                    return (
+                                        <Button
+                                            key={num}
+                                            disabled={pageNumber === num}
+                                            onClick={(e) => clickPageNumberButton(num)}
+                                        >
+                                            {num}
+                                        </Button>
+                                    );
+                                })}
+                                <Button
+                                    disabled={pageNumber === pageNumbers[pageNumbers.length - 1]}
+                                    onClick={(e) => clickPageNumberButton(pageNumber + 1)}
+                                >
+                                    Next
+                                </Button>
+                            </ButtonGroup>
+                        </Box>
+                    )}
+                </Box>
+                :
+                <Box sx={purchasedListTableStyle}>No Feedback Received</Box>}
+
+        </Fragment>
+    );
+};
+
+export default DashboardReviewList;
